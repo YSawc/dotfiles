b3fd851e8013809c8ccd0427e437cf1b9cfa7a4e
YSawc <niceage6690@gmail.com>
2019-12-08 21:38:40 +0900

    small change

--- link.sh
+++ link.sh
@@ -7,7 +7,6 @@ for f in .??*
 do
     [ "$f" = ".git" ] && continue
     [ "$f" = ".config" ] && continue
-    [ "$f" = "neosnipet-snippets" ] && continue
-    ln -n ${DOT_DIRECTORY}/${f} ${HOME}/${f}
+    ln -sfn ${DOT_DIRECTORY}/${f} ${HOME}/${f}
 done
 echo "Linked directory dotfiles!"
