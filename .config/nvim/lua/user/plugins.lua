28fa688f61a87a3ef68f239e38ebcef6eab8fa56
ysawc <niceage6690@gmail.com>
2023-11-27 08:11:06 +0900

    chore: change tags of indent-blankline

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -224,15 +224,9 @@ require('lazy').setup({
 	},
 
 	{
-		'lukas-reineke/indent-blankline.nvim',
-		config = function()
-			require('indent_blankline').setup {
-				show_current_context = true,
-				show_current_context_start = true,
-				show_end_of_line = true,
-				space_char_blankline = ' ',
-			}
-		end
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		opts = {}
 	},
 
 	'RRethy/vim-illuminate',
