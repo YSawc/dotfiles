5205b53c71c5f460e31b19235747b73b7e447fc1
YSawc <niceage6690@gmail.com>
2024-07-29 06:17:58 +0900

    modify: set normal highlight under inactive-code in rust-analyzer.

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -41,8 +41,32 @@ require('lazy').setup({
   },
   {
     'mrcjkb/rustaceanvim',
-    version = '^4', -- Recommended
+    version = '^5', -- Recommended
     lazy = false,   -- This plugin is already lazy
+    config = function()
+      vim.g.rustaceanvim = {
+        -- Plugin configuration
+        tools = {
+        },
+        -- LSP configuration
+        server = {
+          on_attach = function(client, bufnr)
+            -- you can also put keymaps in here
+          end,
+          default_settings = {
+            -- rust-analyzer language server configuration
+            ['rust-analyzer'] = {
+              diagnostics = {
+                disabled = { "inactive-code" }
+              }
+            },
+          },
+        },
+        -- DAP configuration
+        dap = {
+        },
+      }
+    end
   },
   'MunifTanjim/prettier.nvim',
   {
